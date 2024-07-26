1b099884db804805b30b7a311f0546842cc4d391
danidana2 <150885774+danidana2@users.noreply.github.com>
2024-07-27 00:43:23 +0900

    DB기반 로그인 검증 로직
    UserRepository에 회원 조회 메소드 추가
    
    UserDetails, UserDetailsService 커스텀 구현 -> CustomUserDetails, CustomUserDetailsService

--- SomServer/src/main/java/com/example/somserver/repository/UserRepository.java
+++ SomServer/src/main/java/com/example/somserver/repository/UserRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //데이터베이스에 접근할 수 있는 리포지터리
 public interface UserRepository extends JpaRepository<UserEntity, String> { //id 값의 레퍼런스 타입
 
-    Boolean existsByUserId(String userId); //existsBy 구문
+    //특정 userId가 데이터베이스에 존재하는지 여부를 확인하는 메소드 작성
+    Boolean existsByUserId(String userId);
+
+    //userId을 받아 DB 테이블에서 회원을 조회하는 메소드 작성
+    UserEntity findByUserId(String userId);
 }
---
+++ SomServer/src/main/java/com/example/somserver/service/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.example.somserver.service;
+
+import com.example.somserver.dto.CustomUserDetails;
+import com.example.somserver.entity.UserEntity;
+import com.example.somserver.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    //데이터베이스 연결 진행해야함 -> 데이터베이스에 접근할 리포지터리 연결을 위해 주입 받음
+    //생성자 방식으로 UserRepository 주입 받음
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    //데이터 베이스에서 특정 유저를 조회해서 리턴
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+
+        //DB에서 조회
+        UserEntity userData = userRepository.findByUserId(userId);
+
+        if (userData != null) {
+
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
