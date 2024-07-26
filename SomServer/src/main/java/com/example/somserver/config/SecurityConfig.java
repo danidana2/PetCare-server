112ddb90e04e5cca6cec49f082a4b1bd566ba22d
danidana2 <150885774+danidana2@users.noreply.github.com>
2024-07-26 23:07:20 +0900

    로그인 필터 구현
    로그인 검증을 위한 커스텀 UsernamePasswordAuthentication 필터 LoginFilter 작성
    
    SecurityConfig에 커스텀 로그인 필터 등록

--- SomServer/src/main/java/com/example/somserver/config/SecurityConfig.java
+++ SomServer/src/main/java/com/example/somserver/config/SecurityConfig.java
@@ -1,16 +1,35 @@
 package com.example.somserver.config;
 
+import com.example.somserver.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    //AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     //비밀번호 암호화를 위해 BCryptPaasswordEncoder 등록
     @Bean
@@ -39,6 +58,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .anyRequest().authenticated()); //나머지 경로에 대해서는 로그인한 사용자만 접근가능
+
+        //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
 
         //세션 설정
         http
