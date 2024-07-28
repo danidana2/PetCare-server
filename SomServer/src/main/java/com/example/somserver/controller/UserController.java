6e072cc9a82ad88b316b0a31e47f7be5999090b4
danidana2 <150885774+danidana2@users.noreply.github.com>
2024-07-29 02:37:59 +0900

    api 작성 - /users
    UserController, UserService
    
    1.회원가입: DB의 users 테이블에 user_id, password, nickname, role값 저장
    POST
    http://{SERVER_URL}/join
    "userId"
    "password"
    "nickname"
    
    성공-> String "success", Sommungchi DB의 users 테이블에 user_id, password, nickname, role값 저장됨
    실패(중복아이디인 경우)-> String "fail"
    ------------------------------------------------------------------------------------------------------------------
    2.로그인
    POST
    http://{SERVER_URL}/login
    "userId"
    "password"
    
    성공-> 응답 헤더에 "Authorization": "Bearer "+token, "userId": userId
             응답코드 200
    실패-> 응답코드 401
    ------------------------------------------------------------------------------------------------------------------
    3.nickname update: nickname 수정
    PATCH
    http://{SERVER_URL}/users/{userId}/nickname
    {userId}부분에 수정하려는 nickname의 userId 넣기
    "nickname"
    
    성공-> String "success"
    실패-> String "fail"
    ------------------------------------------------------------------------------------------------------------------
    4.password update: password 수정
    PATCH
    http://{SERVER_URL}/users/{userId}/password
    {userId}부분에 수정하려는 password의 userId 넣기
    "password"
    
    성공-> String "success"
    실패-> String "fail"
    ------------------------------------------------------------------------------------------------------------------
    5.nickname 조회
    GET
    http://{SERVER_URL}/users/{userId}/nickname
    {userId} 부분에 조회하려는 nickname의 userId 넣기
    
    성공-> String {nickname}
    실패-> String "fail"
    ------------------------------------------------------------------------------------------------------------------
    6.password check: 확인하고자하는 password가 저장되어있는 password와 같은지 비교
    POST
    http://{SERVER_URL}/users/check/password
    "userId"
    "password" - 확인하고자하는 password
    
    확인하고자하는 password = 저장되어있는 password-> String "success"
    확인하고자하는 password != 저장되어있는 password, error-> String "fail"
    ------------------------------------------------------------------------------------------------------------------
    7.user delete: 유저 탈퇴시 해당 유저 정보 삭제
    DELETE
    http://{SERVER_URL}/users/{userId}
    {userId} 부분에 삭제하려는 유저의 userId 넣기
    
    성공-> String "success"
    실패-> String "fail"

--- SomServer/src/main/java/com/example/somserver/controller/UserController.java
+++ SomServer/src/main/java/com/example/somserver/controller/UserController.java
@@ -1,16 +1,86 @@
 package com.example.somserver.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.somserver.dto.JoinDTO;
+import com.example.somserver.dto.UserDTO;
+import com.example.somserver.service.JoinService;
+import com.example.somserver.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@ResponseBody
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/user")
+    //UserService 주입 받기
+    private final UserService userService;
+
+    public UserController(UserService userService) { //생성자 방식으로
+
+        this.userService = userService;
+    }
+
+    @GetMapping
     public String userP() {
 
         return "user controller";
+    }
+
+    //nickname update api
+    @PatchMapping("/{userId}/nickname")
+    public String updateNickname(@PathVariable String userId, UserDTO userDTO) {
+
+        boolean updateResult = userService.updateNickname(userId, userDTO);
+
+        if (!updateResult) { //updateResult=false인 경우
+            return "fail";
+        }
+        return "success";
+    }
+
+    //password update api
+    @PatchMapping("{userId}/password")
+    public String updatePassword(@PathVariable String userId, UserDTO userDTO) {
+
+        boolean updateResult = userService.updatePassword(userId, userDTO);
+
+        if (!updateResult) { //updateResult=false인 경우
+            return "fail";
+        }
+        return "success";
+    }
+
+    //nickname 조회 api
+    @GetMapping("/{userId}/nickname")
+    public String getNickname(@PathVariable String userId) {
+
+        String nickname = userService.getNicknameByUserId(userId);
+
+        if (nickname.equals("error")) { //updateResult=false인 경우
+            return "fail";
+        }
+        return nickname;
+    }
+
+    //password check api
+    @PostMapping("/check/password")
+    public String checkPassword(UserDTO userDTO) {
+
+        boolean checkResult = userService.checkPassword(userDTO);
+
+        if (!checkResult) { //checkResult=false인 경우
+            return "fail";
+        }
+        return "success";
+    }
+
+    //user delete api
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId) {
+
+        boolean deleteResult = userService.deleteUser(userId);
+
+        if (!deleteResult) { //deleteResult=false인 경우
+            return "fail";
+        }
+        return "success";
     }
 }
---
+++ SomServer/src/main/java/com/example/somserver/service/UserService.java
@@ -0,0 +1,120 @@
+package com.example.somserver.service;
+
+import com.example.somserver.dto.UserDTO;
+import com.example.somserver.entity.UserEntity;
+import com.example.somserver.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Service
+public class UserService {
+
+    //UserRepository 주입 받기 //BCryptPasswordEncoder 주입 받기
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    //nickname update api
+    public boolean updateNickname(String userId, UserDTO userDTO) {
+
+        try {
+            //UserDTO 에서 nickname 값 꺼내야함
+            String nickname = userDTO.getNickname();
+
+            //UserEntity에 dto에서 받은 DATA를 옮겨주기 위해서
+            UserEntity data = userRepository.findByUserId(userId);
+            data.setNickname(nickname);
+
+            userRepository.save(data);
+
+            return true;
+        } catch (Exception e) {
+            //예외가 발생하면 false 반환
+            return false;
+        }
+    }
+
+    //password update api
+    public boolean updatePassword(String userId, UserDTO userDTO) {
+
+        try {
+            //UserDTO 에서 password 값 꺼내야함
+            String password = userDTO.getPassword();
+
+            //UserEntity에 dto에서 받은 DATA를 옮겨주기 위해서
+            UserEntity data = userRepository.findByUserId(userId);
+            data.setPassword(bCryptPasswordEncoder.encode(password));
+
+            userRepository.save(data);
+
+            return true;
+        } catch (Exception e) {
+            //예외가 발생하면 false 반환
+            return false;
+        }
+    }
+
+    //nickname 조회 api
+    public String getNicknameByUserId(String userId){
+
+        try {
+            //nickname
+            String nickname;
+
+            //userId로 조회한 UserEntity에서 nickname 얻기
+            UserEntity data = userRepository.findByUserId(userId);
+            nickname = data.getNickname();
+
+            return nickname;
+        } catch (Exception e) {
+            //예외가 발생하면 false 반환
+            return "error";
+        }
+    }
+
+    //password check api
+    public boolean checkPassword(UserDTO userDTO) {
+
+        try {
+            //UserDTO 에서 userId, password 값 꺼내야함
+            String userId = userDTO.getUserId();
+            String password = userDTO.getPassword();
+
+            //저장된 password를 저장할 myPassword
+            String myPassword;
+
+            //userId로 조회한 UserEntity에서 password 얻기
+            UserEntity data = userRepository.findByUserId(userId);
+            myPassword = data.getPassword();
+
+            //비밀번호 비교
+            boolean isSame;
+            isSame = bCryptPasswordEncoder.matches(password, myPassword);
+
+            return isSame;
+        } catch (Exception e) {
+            //예외가 발생하면 false 반환
+            return false;
+        }
+    }
+
+    //user delete api
+    public boolean deleteUser(String userId) {
+
+        try {
+            //데이터베이스에서 해당 userId를 가진 사용자를 삭제
+            userRepository.deleteById(userId);
+
+            return true;
+        } catch (Exception e) {
+            //예외가 발생하면 false 반환
+            return false;
+        }
+    }
+}
